01344d7fab77999f43e84186829a9b2a065291dd
Jake Soloff <jakesoloff@gmail.com>
2023-09-13 00:27:04 -0500

    init relative

--- npeb/__init__.py
+++ npeb/__init__.py
@@ -1,3 +1,3 @@
-from Grenander import Grenander
-from PMixture import PMixture
-from GLMixture import GLMixture
+from npeb.Grenander import Grenander
+from npeb.PMixture import PMixture
+from npeb.GLMixture import GLMixture
